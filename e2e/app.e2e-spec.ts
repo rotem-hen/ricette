a6d48d35591205ff25714d55004d9f3eea5498e3
Rotem Hen <rotemy9@gmail.com>
2020-04-18 16:02:17 +0300

    Change name to Ricette

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { MyRecipesPage } from './app.po';
+import { RicettePage } from './app.po';
 
 describe('my-recipes App', () => {
-  let page: MyRecipesPage;
+  let page: RicettePage;
 
   beforeEach(() => {
-    page = new MyRecipesPage();
+    page = new RicettePage();
   });
 
   it('should display message saying app works', () => {
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, element, by } from 'protractor';
 
-export class MyRecipesPage {
+export class RicettePage {
   navigateTo() {
     return browser.get('/');
   }
