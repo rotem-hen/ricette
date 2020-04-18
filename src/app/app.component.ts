f634d20bc723f7921e87cba1f373620f3dcdaab4
Rotem Hen <rotemy9@gmail.com>
2020-04-18 15:57:53 +0300

    Added basic components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'why is everything hard';
+  title = 'Why is Everything Hard?';
 }
