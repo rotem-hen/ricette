7229af167480c8164a6d3970e1aef740be43f124
Rotem Hen <rhen@salesforce.com>
2020-04-24 18:23:48 +0300

    preparation for recipe page

--- src/app/db.ts
+++ src/app/db.ts
@@ -47,26 +47,152 @@ export const data = {
     }
   ],
   recipes: [
-    { id: 1, title: 'קציצות דגים', categories: [2, 5] },
-    { id: 2, title: 'דניס בחמאת לימון', categories: [2, 5] },
-    { id: 3, title: 'לזניית בטטה', categories: [3, 7, 8] },
-    { id: 4, title: 'סלט אורז קר ובטטות צלויות', categories: [3, 8] },
-    { id: 5, title: 'חומיות', categories: [4, 8] },
-    { id: 6, title: 'עוגת ביסקוויטים', categories: [4, 8] },
-    { id: 7, title: 'עוגת לוטוס', categories: [4, 8] },
-    { id: 8, title: 'פיצה עם פפרוני', categories: [3, 7] },
-    { id: 9, title: 'פסטה ארביאטה', categories: [2, 3, 7, 8] },
-    { id: 10, title: 'פסטה ראגו', categories: [2, 3, 7] },
-    { id: 11, title: 'פילה סלמון', categories: [2, 5] },
-    { id: 12, title: 'רצועות חזה עוף בלימון ודבש', categories: [] },
-    { id: 13, title: 'מרק עדשים', categories: [3, 6, 8] },
-    { id: 14, title: 'מרק פטריות', categories: [3, 6, 8] },
-    { id: 15, title: 'שקשוקה מקסיקנית', categories: [] },
-    { id: 16, title: 'בנדיקט', categories: [3, 8] },
-    { id: 17, title: 'אפוגטו', categories: [4, 7, 8] },
-    { id: 18, title: "סביצ'ה", categories: [2, 3, 5] },
-    { id: 19, title: 'בורקס', categories: [3, 8] },
-    { id: 20, title: 'ראמן', categories: [2, 3, 6] },
-    { id: 21, title: 'ריזוטו', categories: [3, 7, 8] }
+    {
+      id: 1,
+      title: 'קציצות דגים',
+      categories: [2, 5],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 2,
+      title: 'דניס בחמאת לימון',
+      categories: [2, 5],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 3,
+      title: 'לזניית בטטה',
+      categories: [3, 7, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 4,
+      title: 'סלט אורז קר ובטטות צלויות',
+      categories: [3, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 5,
+      title: 'חומיות',
+      categories: [4, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 6,
+      title: 'עוגת ביסקוויטים',
+      categories: [4, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 7,
+      title: 'עוגת לוטוס',
+      categories: [4, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 8,
+      title: 'פיצה עם פפרוני',
+      categories: [3, 7],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 9,
+      title: 'פסטה ארביאטה',
+      categories: [2, 3, 7, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 10,
+      title: 'פסטה ראגו',
+      categories: [2, 3, 7],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 11,
+      title: 'פילה סלמון',
+      categories: [2, 5],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 12,
+      title: 'רצועות חזה עוף בלימון ודבש',
+      categories: [],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 13,
+      title: 'מרק עדשים',
+      categories: [3, 6, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 14,
+      title: 'מרק פטריות',
+      categories: [3, 6, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 15,
+      title: 'שקשוקה מקסיקנית',
+      categories: [],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 16,
+      title: 'בנדיקט',
+      categories: [3, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 17,
+      title: 'אפוגטו',
+      categories: [4, 7, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 18,
+      title: "סביצ'ה",
+      categories: [2, 3, 5],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 19,
+      title: 'בורקס',
+      categories: [3, 8],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 20,
+      title: 'ראמן',
+      categories: [2, 3, 6],
+      ingredients: [],
+      prep: []
+    },
+    {
+      id: 21,
+      title: 'ריזוטו',
+      categories: [3, 7, 8],
+      ingredients: [],
+      prep: []
+    }
   ]
 };
