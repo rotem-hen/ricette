e48daef7a60d02acf431226d9a98c068f8d0d98c
Rotem Hen <rotemy9@gmail.com>
2020-04-25 22:47:11 +0300

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { TopPanelComponent } from './top-panel/top-panel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { AddButtonComponent } from './add-button/add-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ContentModule } from './content/content.module';
+import { NavbarItemComponent } from './navbar-item/navbar-item.component';
 
 @NgModule({
-  declarations: [AppComponent, TopPanelComponent, AddButtonComponent],
+  declarations: [AppComponent, NavbarComponent, AddButtonComponent, NavbarItemComponent],
   imports: [BrowserModule, FormsModule, FontAwesomeModule, ContentModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.spec.ts
+++ src/app/navbar/navbar.component.spec.ts
@@ -1,20 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TopPanelComponent } from './top-panel.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('TopPanelComponent', () => {
-  let component: TopPanelComponent;
-  let fixture: ComponentFixture<TopPanelComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TopPanelComponent ]
-    })
-    .compileComponents();
+      declarations: [NavbarComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TopPanelComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
