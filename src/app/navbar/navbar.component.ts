2dd7c4a997471f2ae461dbf0811b55453947e08e
Rotem Hen <rotemy9@gmail.com>
2020-04-29 21:03:02 +0300

    small navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  public menuShown = false;
+
+  public onMenuClick(): void {
+    this.menuShown = !this.menuShown;
+  }
+}
