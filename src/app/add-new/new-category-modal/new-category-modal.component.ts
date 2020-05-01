2ddbbc7034bb78de94dec7a4ddb85efc98386002
Rotem Hen <rotemy9@gmail.com>
2020-05-01 20:32:10 +0300

    new-catogory-modal

--- src/app/add-new/new-category-modal/new-category-modal.component.ts
+++ src/app/add-new/new-category-modal/new-category-modal.component.ts
@@ -1,17 +1,47 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { data } from '../../db';
+import { Recipe } from 'app/content/interface/recipe.interface';
 
 @Component({
   selector: 'app-new-category-modal',
   templateUrl: './new-category-modal.component.html',
   styleUrls: ['./new-category-modal.component.css']
 })
-export class NewCategoryModalComponent {
+export class NewCategoryModalComponent implements OnInit {
   @ViewChild('newCategoryModal') modalRef: ElementRef;
+  public categoryNameInput: string;
 
+  options: {
+    recipe: Recipe;
+    selected: boolean;
+  }[];
   constructor(private modalService: NgbModal) {}
+
+  public ngOnInit(): void {
+    this.options = data.recipes.map(recipe => ({ recipe, selected: false }));
+  }
 
   public open(): void {
     this.modalService.open(this.modalRef, { scrollable: true });
+  }
+
+  public onOK(modal, categoryColor): void {
+    modal.close('Ok click');
+    const id = 200;
+    data.categories.push({ id, name: this.categoryNameInput, color: categoryColor });
+    data.recipes.forEach((recipe: Recipe) => {
+      if (this.isRecipeSelected(recipe.id)) {
+        recipe.categories.push(id);
+      }
+    });
+  }
+
+  public onCategoryNameInputChange(event): void {
+    this.categoryNameInput = event.target.value;
+  }
+
+  private isRecipeSelected(id): boolean {
+    return this.options.find(o => o.recipe.id === id).selected;
   }
 }
