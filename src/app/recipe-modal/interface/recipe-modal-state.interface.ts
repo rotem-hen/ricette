a5830bb52fb4a7ccdfd770ef6f33a3f73a7ce8fa
Rotem Hen <rhen@salesforce.com>
2020-05-06 20:04:58 +0300

    change indredients and prep type to string + more modal

--- src/app/recipe-modal/interface/recipe-modal-state.interface.ts
+++ src/app/recipe-modal/interface/recipe-modal-state.interface.ts
@@ -3,8 +3,8 @@ import { Category } from 'app/content/interface/category.interface';
 export interface RecipeModalState {
   title: string;
   isFavourite: boolean;
-  ingredients: string[];
-  prep: string[];
+  ingredients: string;
+  prep: string;
   image: string;
   options: {
     category: Category;
