6488c6d09b3b4fd518d734b7d99a6179e562d7f4
Rotem Hen <rotemy9@gmail.com>
2020-05-13 14:05:05 +0300

    updating route action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Router, NavigationEnd } from '@angular/router';
+import { EditModeService } from './shared/edit-mode-service/edit-mode.service';
+import { Scroller } from './shared/scroll-top';
+import { SearchService } from './shared/search-service/search.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+  private destroy$ = new Subject();
+
+  constructor(
+    private router: Router,
+    private editModeService: EditModeService,
+    private scroller: Scroller,
+    private searchService: SearchService
+  ) {
+    router.events.pipe(takeUntil(this.destroy$)).subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        this.scroller.scrollTop();
+        this.editModeService.toggleEditMode(false);
+        if (val.url !== '/categories/3000') this.searchService.setSearchTerm('');
+      }
+    });
+  }
+
   title = 'Why is Everything Hard?';
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
