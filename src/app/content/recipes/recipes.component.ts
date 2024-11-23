98fd9e2645897476f5387134fd11211416bc12b8
Rotem Hen <rotemy9@gmail.com>
2024-11-23 14:08:49 +0200

    fix english search

--- src/app/content/recipes/recipes.component.ts
+++ src/app/content/recipes/recipes.component.ts
@@ -65,9 +65,15 @@ export class RecipesComponent implements OnInit, OnDestroy {
 
   private applySearch(value: string): void {
     if (this.categoryId !== SpecialCategories.SEARCH_RESULTS) return;
-    history.replaceState(history.state, '', `categories/${SpecialCategories.SEARCH_RESULTS}?${this.QUERY_PARAM}=${value}`);
+    history.replaceState(
+      history.state,
+      '',
+      `categories/${SpecialCategories.SEARCH_RESULTS}?${this.QUERY_PARAM}=${value}`
+    );
     this.categoryName = `${this.searchCategoryName}: ${value}`;
-    this.recipeList = this.allRecipes.filter(r => `${r.title}\n${r.ingredients}`.includes(value));
+    this.recipeList = this.allRecipes.filter(r =>
+      `${r.title}\n${r.ingredients}`.toLowerCase().includes(value.toLowerCase())
+    );
   }
 
   public onEditCategory(modalRef): () => void {
